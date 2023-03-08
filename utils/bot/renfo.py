0bd9a070c055bd10b4b400464dc5162434a164d7
Ukenn <duola2002@gmail.com>
2023-03-08 18:05:54 +0900

    批量生成 NFO 的错误跳过

--- utils/bot/renfo.py
+++ utils/bot/renfo.py
@@ -37,12 +37,13 @@ async def re_subject_nfo(message: Message, bot: AsyncTeleBot):
         worker_path = f"{config['save_path']}{up_folder_name}_nfo"
         if not os.path.exists(worker_path): os.mkdir(worker_path)
         # 生成 Season NFO
-        if tmdb_d:
+        if tmdb_d and tmdb_d != "None":
             try:
                 subject_data = subject_nfo(bgm_id, tmdb_d)
                 save_nfo(worker_path, subject_data=subject_data)
             except Exception as e:
-                return await bot.reply_to(message, f"[{i+1}/{todo_num}] 获取生成 Season NFO 数据失败:\n\n{e}")
+                await bot.reply_to(message, f"[{i+1}/{todo_num}] {bgm_id}:{tmdb_d} 获取生成 Season NFO 数据失败:\n\n{e}")
+                continue
             await bot.edit_message_text(f"[{i+1}/{todo_num}] 已生成 Season NFO 文件，正在开始生成 Episode NFO...", message.chat.id, msg.message_id)
         try:
             proc = await asyncio.create_subprocess_exec(
@@ -51,7 +52,8 @@ async def re_subject_nfo(message: Message, bot: AsyncTeleBot):
             output = await proc.communicate()
             dirs_list = [i['Name'] for i in json.loads(output[0].decode("utf-8")) if 'video' in i['MimeType']]
         except:
-            return await bot.reply_to(message, f"[{i+1}/{todo_num}] 获取文件列表失败: 未找到文件夹 {up_folder_name}")
+            await bot.reply_to(message, f"[{i+1}/{todo_num}] {bgm_id}:{tmdb_d} 获取文件列表失败: 未找到文件夹 {up_folder_name}")
+            continue
         if dirs_list:
             for video in dirs_list:
                 try:
@@ -63,10 +65,12 @@ async def re_subject_nfo(message: Message, bot: AsyncTeleBot):
                     episode_data = episode_nfo(bgm_id, volume, episode_type)
                     save_nfo(worker_path, episode_data=episode_data, video_name=video.split('.')[0])
                 except Exception as e:
-                    return await bot.reply_to(message, f"[{i+1}/{todo_num}] 读取文件列表失败: 获取生成 Episode NFO 数据出错或命名错误\n\n{e}")
+                    await bot.reply_to(message, f"[{i+1}/{todo_num}] {bgm_id}:{tmdb_d} 读取文件列表失败: 获取生成 Episode NFO 数据出错或命名错误\n\n{e}")
+                    continue
             await bot.edit_message_text(f"[{i+1}/{todo_num}] 已生成 Episode NFO 文件，正在开始上传...", message.chat.id, msg.message_id)
         else:
-            return await bot.reply_to(message, f"[{i+1}/{todo_num}] 获取文件列表失败: 未找到 {up_folder_name} 文件夹下的视频文件")
+            await bot.reply_to(message, f"[{i+1}/{todo_num}] {bgm_id}:{tmdb_d} 获取文件列表失败: 未找到 {up_folder_name} 文件夹下的视频文件")
+            continue
         try:
             proc = await asyncio.create_subprocess_exec(
                 "rclone", "move", worker_path,
@@ -76,6 +80,6 @@ async def re_subject_nfo(message: Message, bot: AsyncTeleBot):
             if proc.returncode == 0:
                 await bot.edit_message_text(f"[{i+1}/{todo_num}] 已生成并上传成功", message.chat.id, msg.message_id)
         except Exception as e:
-            await bot.reply_to(message, f"[{i+1}/{todo_num}] 上传失败: {e}")
+            await bot.reply_to(message, f"[{i+1}/{todo_num}] {bgm_id}:{tmdb_d} 上传失败: {e}")
         finally:
             shutil.rmtree(worker_path)
